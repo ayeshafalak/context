39c4580660d900a77e6bc7e970c4a6e543d1bf4f
Ayesha Falak <60551252+ayeshafalak@users.noreply.github.com>
2023-10-26 18:23:19 +0530

    Add files via upload

--- src/Components/GrandChildComponent.js
+++ src/Components/GrandChildComponent.js
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
 import './style.css';
+import { useMyContext } from '../MyContext';
 
 function GrandChildComponent({isClicked}) {
+  const { isCtxClicked } = useMyContext();
 
   return (
     <div className="box red">
       <h3>GrandChildComponent Component</h3>
       <p>The GrandChildComponent displayes some text based on the value of isClicked state.</p>
-      <p>{isClicked? "The button was clicked" : "The button is not clicked."}</p>
+      <p>{isClicked? "Prop drilled down" : ""}</p>
+      <p>{isCtxClicked? "CTX clicked" : ""}</p>
+
     </div>
   );
 }
--- src/Components/ParentComponent.js
+++ src/Components/ParentComponent.js
@@ -1,19 +1,28 @@
 import React, { useState } from 'react';
 import './style.css';
 import ChildComponent from './ChildComponent';
+import { useMyContext } from '../MyContext';
 
 function ParentComponent() {
   const [isClicked, setClicked] = useState(false);
-  
+  const {setCTxClicked} = useMyContext();
+
 const handleButtonClick = () =>{
     setClicked(true);
 }
+
+const handleCtxButtonClick = () =>{
+  setCTxClicked(true);
+}
+
 
   return (
     <div className="box">
       <h3>Parent Component</h3>
       <p>We have a state called 'isClicked' whis set to false. It will be set to true when button is clicked.</p>
       <button onClick={handleButtonClick}>Reveal</button>
+      <button onClick={handleCtxButtonClick}>Ctx</button>
+
       <ChildComponent isClicked={isClicked} />
     </div>
 
