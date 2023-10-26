30507164ec39f4912ee06950dbedeb1c0f847df2
Ayesha Falak <60551252+ayeshafalak@users.noreply.github.com>
2023-10-26 18:22:48 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import ParentComponent from './Components/ParentComponent';
+import { MyContextProvider } from './MyContext';
 
 function App() {
   return (
     <div className="App">
      <h1>WELCOME TO THE TUTORIAL OF PROP DRILLING & CONTEXT API at e-Labs</h1>
-     <ParentComponent />
+     <MyContextProvider>
+       <ParentComponent />
+     </MyContextProvider>
+    
 
     </div>
   );
