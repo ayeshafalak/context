bbc1db3f2b7dbe98adaa7e128502ee3c8cb7bba2
Ayesha Falak <60551252+ayeshafalak@users.noreply.github.com>
2023-10-26 17:36:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ParentComponent from './Components/ParentComponent';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>WELCOME TO THE TUTORIAL OF PROP DRILLING & CONTEXT API at e-Labs</h1>
+     <ParentComponent />
+
+    </div>
+  );
+}
+
+export default App;
